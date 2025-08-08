e1f6d1b126bce925bf0d732e29d0c5d369665874
H7KZ <74021016+H7KZ@users.noreply.github.com>
2025-08-08 23:57:05 +0200

    feat: Barča

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -2,23 +2,24 @@
 	import type { Snippet } from 'svelte';
 
 	interface Props {
+		type?: 'button' | 'submit' | 'reset';
 		link?: string;
 		target?: string;
 		onclick?: (e: MouseEvent) => void;
 		children: Snippet<[]>;
 	}
 
-	let { link, target, onclick, children }: Props = $props();
+	let { type, link, target, onclick, children }: Props = $props();
 </script>
 
 {#snippet Button()}
-	<button class="cursor-pointer rounded-full border-[1.5px] border-white px-6 py-1.25 transition-all hover:bg-[#ffffff18]">
+	<button type={type} {onclick} class="cursor-pointer rounded-full border-[1.5px] border-white px-6 py-1.25 transition-all hover:bg-[#ffffff18]">
 		{@render children()}
 	</button>
 {/snippet}
 
 {#if link}
-	<a href={link} {target} {onclick}>
+	<a href={link} {target}>
 		{@render Button()}
 	</a>
 {:else}
---
+++ src/routes/fragment/+page.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import { goto } from '$app/navigation';
+	import Button from '$lib/components/Button.svelte';
+	import TextInput from '$lib/components/inputs/TextInput.svelte';
+	import { _ } from 'svelte-i18n';
+
+	let form = $state({
+		code: ''
+	});
+
+	async function handleSubmit(e: MouseEvent) {
+		console.log('Form submitted:', form);
+		e.preventDefault();
+
+		if (form.code !== '211') return
+
+		await goto('/fragment/1.txt');
+	}
+</script>
+
+<div class="flex w-full pt-32 flex-col justify-center items-center gap-3">
+	<div class="w-full max-w-xs px-4 flex flex-col gap-8">
+		<TextInput
+			bind:value={form.code}
+			label="Ověřovací kód"
+			required
+		/>
+		<Button onclick={handleSubmit}>
+			Odemknout
+		</Button>
+	</div>
+</div>
